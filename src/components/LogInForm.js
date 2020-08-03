f556a807df798f2375043a40697343b8a026dbea
Chris K <ikim1991@gmail.com>
2020-08-03 14:55:23 -0400

    Adding Loading Spinner on Log In

--- src/components/LogInForm.js
+++ src/components/LogInForm.js
@@ -6,7 +6,8 @@ import { loggingIn } from '../actions';
 const mapStateToProps = (state) => {
   return {
     user: state.userLogIn.user,
-    game: state.gameState.game
+    game: state.gameState.game,
+    isPending: state.userLogIn.isPending
   }
 }
 
@@ -37,30 +38,37 @@ function LogInForm(props) {
 
   return (
     <div className="login-form">
-      <div className="login-main bg-near-white ba br2">
-        <div className="login-title">
-          <h1>PLAY BLACKJACK</h1>
-          <h2>Winner, Winner Chicken Dinner!</h2>
-        </div>
-        <div className="mt3">
-          <label className="db mb1" htmlFor="username">Username</label>
-          <input id="login-username" className="pa2 ba mb3 bg-transparent hover-bg-black hover-white w-60" type="text" name="username" defaultValue="admin" autoFocus/>
-          <label className="db mb1" htmlFor="username">Password</label>
-          <input id="login-password" className="pa2 ba mb3 bg-transparent hover-bg-black hover-white w-60" type="password" name="password" defaultValue="admin"/>
-          <label className="db mb1" htmlFor="servers">Choose Server</label>
-          <select id="servers" className="pa2 ba bg-transparent hover-bg-black hover-white pointer w-60" name="servers" defaultValue="server-1">
-            <option value="default"></option>
-            <option value="server-1">Server 1</option>
-            <option value="server-2">Server 2</option>
-            <option value="server-3">Server 3</option>
-            <option value="server-4">Server 4</option>
-          </select>
-          <div className="buttons">
-            <button className="mt3 mb3 w-20 pa2 center ba bg-transparent hover-bg-black hover-white pointer button" onClick={onConnect}>Connect</button>
-            <button className="mt3 mb3 w-20 pa2 center ba bg-transparent hover-bg-black hover-white pointer button" onClick={() => props.history.push("/signup")}>Sign Up</button>
+      {
+        (!props.isPending) ? (
+          <div className="login-main bg-near-white ba br2">
+            <div className="login-title">
+              <h1>PLAY BLACKJACK</h1>
+              <h2>Winner, Winner Chicken Dinner!</h2>
+            </div>
+            <div className="mt3">
+              <label className="db mb1" htmlFor="username">Username</label>
+              <input id="login-username" className="pa2 ba mb3 bg-transparent hover-bg-black hover-white w-60" type="text" name="username" defaultValue="admin" autoFocus/>
+              <label className="db mb1" htmlFor="username">Password</label>
+              <input id="login-password" className="pa2 ba mb3 bg-transparent hover-bg-black hover-white w-60" type="password" name="password" defaultValue="admin"/>
+              <label className="db mb1" htmlFor="servers">Choose Server</label>
+              <select id="servers" className="pa2 ba bg-transparent hover-bg-black hover-white pointer w-60" name="servers" defaultValue="server-1">
+                <option value="default"></option>
+                <option value="server-1">Server 1</option>
+                <option value="server-2">Server 2</option>
+                <option value="server-3">Server 3</option>
+                <option value="server-4">Server 4</option>
+              </select>
+              <div className="buttons">
+                <button className="mt3 mb3 w-20 pa2 center ba bg-transparent hover-bg-black hover-white pointer button" onClick={onConnect}>Connect</button>
+                <button className="mt3 mb3 w-20 pa2 center ba bg-transparent hover-bg-black hover-white pointer button" onClick={() => props.history.push("/signup")}>Sign Up</button>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+        ) : (
+          <div className="loader"></div>
+        )
+      }
+
     </div>
   )
 }
